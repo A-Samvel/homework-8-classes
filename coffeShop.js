945cf6b57a00dd6fee404f126bff34c9e9006c0f
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-10-08 20:02:54 +0400

    add coffeShop method addOrder

---
+++ coffeShop.js
@@ -0,0 +1,95 @@
+const generalMenu = [
+  {
+    foodName: "orange juice",
+    type: "drink",
+    price: 250,
+  },
+  {
+    foodName: "lemonade",
+    type: "drink",
+    price: 50,
+  },
+  {
+    foodName: "cranberry juice",
+    type: "drink",
+    price: 350,
+  },
+  {
+    foodName: "pineapple juice",
+    type: "drink",
+    price: 400,
+  },
+  {
+    foodName: "lemon iced tea",
+    type: "drink",
+    price: 450,
+  },
+  {
+    foodName: "vanilla chai latte",
+    type: "drink",
+    price: 400,
+  },
+  {
+    foodName: "hot chocolate",
+    type: "drink",
+    price: 350,
+  },
+  {
+    foodName: "iced coffee",
+    type: "drink",
+    price: 300,
+  },
+  {
+    foodName: "tuna sandwich",
+    type: "whetherfood",
+    price: 300,
+  },
+  {
+    foodName: "ham and cheese sandwich",
+    type: "whetherfood",
+    price: 350,
+  },
+  {
+    foodName: "bacon and egg",
+    type: "whetherfood",
+    price: 400,
+  },
+  {
+    foodName: "steak",
+    type: "whetherfood",
+    price: 1000,
+  },
+  {
+    foodName: "hamburger",
+    type: "whetherfood",
+    price: 500,
+  },
+  {
+    foodName: "cinnamon roll",
+    type: "whetherfood",
+    price: 350,
+  },
+];
+
+class CoffeeShop {
+  #name;
+  #menu;
+  #orders;
+  #orderNames;
+  constructor(name, menu) {
+    this.#name = name;
+    this.#menu = menu;
+    this.#orders = [];
+    this.#orderNames = [];
+  }
+
+  addOrder(order) {
+    const res = this.#menu.find(({ foodName }) => foodName === order);
+    if (res) {
+      this.#orders.push(res);
+      this.#orderNames.push(res.foodName);
+      return "Order added!";
+    }
+    return "This item is currently unavailable!";
+  }
+}
