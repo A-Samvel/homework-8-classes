93227f5e93cbc2b2bd63725f1f374094d4aad1ec
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-10-08 20:58:34 +0400

    code fix and removal array of test words

--- shiratori.js
+++ shiratori.js
@@ -1,22 +1,3 @@
-const wordArray = [
-  "apple",
-  "ear",
-  "rhino",
-  "corn",
-  "hostess",
-  "stash",
-  "word",
-  "dowry",
-  "yodel",
-  "leader",
-  "righteous",
-  "serpent",
-  "motive",
-  "beach",
-  "hive",
-  "eh",
-];
-
 class Shiritory {
   #gameOver;
   constructor() {
@@ -25,26 +6,24 @@ class Shiritory {
   }
   play(word) {
     word = word.toLowerCase();
-    const validWord = wordArray.find((item) => item === word);
-    if (validWord) {
-      const length = this.words.length;
-      const lastWord = this.words[length - 1];
-      if (length === 0) {
-        this.#gameOver = false;
-        this.words.push(word);
-        return this.words;
-      }
-      if (
-        lastWord[lastWord.length - 1] === word[0] &&
-        this.words.indexOf(word) === -1
-      ) {
-        this.words.push(word);
-        return this.words;
-      } else {
-        this.#gameOver = true;
-        return "game over";
-      }
-    } else throw new Error("Invalid Word");
+    const length = this.words.length;
+    const lastWord = this.words[length - 1];
+
+    if (length === 0) {
+      this.#gameOver = false;
+      this.words.push(word);
+      return this.words;
+    }
+    if (
+      lastWord[lastWord.length - 1] === word[0] &&
+      this.words.indexOf(word) === -1
+    ) {
+      this.words.push(word);
+      return this.words;
+    } else {
+      this.#gameOver = true;
+      return "game over";
+    }
   }
   restart() {
     this.words = [];
@@ -55,12 +34,12 @@ class Shiritory {
 
 const myShiritori = new Shiritory();
 
-console.log(myShiritori.play("apple")); 
-console.log(myShiritori.play("ear")); 
-console.log(myShiritori.play("rhino")); 
-console.log(myShiritori.play("corn")); 
+console.log(myShiritori.play("apple"));
+console.log(myShiritori.play("ear"));
+console.log(myShiritori.play("rhino"));
+console.log(myShiritori.play("corn"));
 
 console.log(myShiritori.words);
 
-console.log(myShiritori.restart()); 
+console.log(myShiritori.restart());
 console.log(myShiritori.words);
