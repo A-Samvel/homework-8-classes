caf416e0a8c1b8fa3de409c5d8cea7ba153e97f7
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-10-10 20:22:59 +0400

    add Book  class,  toString method and code check

--- authorAndPersonClass.js
+++ authorAndPersonClass.js
@@ -33,3 +33,39 @@ class Author {
     return this.#gender === "male" ? ` Mr. ${this.#name}` : `Ms. ${this.#name}`;
   }
 }
+
+class Book {
+  #title;
+  #author;
+  #price;
+  #quantity;
+  constructor(title, author = {}, price, quantity) {
+    this.#title = title;
+    this.#author = author;
+    this.#price = price;
+    this.#quantity = price;
+  }
+
+  get title() {
+    return this.#title;
+  }
+  get author() {
+    return this.#author;
+  }
+  get price() {
+    return this.#price;
+  }
+  get price() {
+    return this.#quantity;
+  }
+
+  toString() {
+    const { name } = this.#author;
+    return `${name} author of "${this.title}"`;
+  }
+}
+const author1 = new Author("J. K. Rowling", "abc@gmail.com", "fEmaLe");
+const book1 = new Book("The Lord of the Ring", author1);
+
+console.log(book1.author.name);
+console.log(book1.toString());
